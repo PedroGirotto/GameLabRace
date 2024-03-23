eb36af8b25de09bfadf11d483da68f1c16219374
PedroGirotto <pedro.girotto@prof.cesupa.br>
2024-03-22 22:10:29 -0300

    Adicionando os créditos no jogo; Fazer uma primeira build.

--- Scenes/UI/CreditButton.cs
+++ Scenes/UI/CreditButton.cs
@@ -1,15 +1,27 @@
 using Godot;
 using System;
 
-public partial class CreditButton : Node
+public partial class CreditButton : Button
 {
+	[Export]
+	public TextEdit creditoEsquerdo { get; set; } = new TextEdit();
+	
+	[Export]
+	public TextEdit creditoDireito { get; set; } = null;
+
+	public bool visibilidade;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		visibilidade = false;
+		Pressed += OnCreditButtonPressed;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void OnCreditButtonPressed()
 	{
+		visibilidade = !visibilidade;
+		creditoDireito.Visible = visibilidade;
+		creditoEsquerdo.Visible = visibilidade;
 	}
 }
